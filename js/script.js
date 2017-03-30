b10d27ff7c0e3e0c5ace326f267dce84276fdcc8
irfanpule <irfan.pule2@gmail.com>
2017-03-30 22:36:59 +0700

    remove mailchimp fucntion

--- js/script.js
+++ js/script.js
@@ -217,31 +217,6 @@ $(document).ready(function() {
 
 
 
-//MAILCHIMP
-$(document).ready(function() {
-
-    "use strict";
-
-    $('#mc-form').ajaxChimp({
-        callback: mailchimpCallback,
-        url: "https://themerocks.us9.list-manage.com/subscribe/post?u=f04c804868966b1b4509daa9b&amp;id=ad7b6aba65"
-    });
-
-    function mailchimpCallback(resp) {
-
-        "use strict";
-
-        if (resp.result === 'success') {
-            $('.subscription-success').html('<i class="pe-7s-check"></i><br/>' + resp.msg).fadeIn(1000);
-            $('.subscription-error').fadeOut(500);
-        } else if (resp.result === 'error') {
-            $('.subscription-error').html('<i class="pe-7s-close-circle"></i><br/>' + resp.msg).fadeIn(1000);
-        }
-    }
-});
-
-
-
 //CONTACT FORM VALIDATION
 $(document).ready(function() {
 
